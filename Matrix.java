604d4bbfc8b0ecfb8d3da2c4611319cd61f368be
pratikbadlani02 <pratikbadlani@gmail.com>
2022-06-13 13:31:39 -0400

    Update Matrix

--- Matrix.java
+++ Matrix.java
@@ -1,27 +1,38 @@
 public class Matrix{
 
-    int n=4;
-    int[][] arr = new int[2*n-1][2*n-1];
-    int counterX = n;
-    int counterY = n;
-    for(int i=0; i< n ; i++) {
-        k = n;
-        for(j=0;j<n;j++) {
-            if(k+i > n)
-                k--;
-            a[i][j] = k;
-        }
-        for(j=n; j>=0; j--) {
+    public static void main(String args[]) {
 
-            a[i][counterX++] = a[i][j];
-        }
-    }
-    for(i=n-2; i>=0;i--) {
-        for(j=0;j< 2*n-1; j++)
-            a[counterY++][j] = a[i][j];            
-    }
+            int n=4;
+            int k;
+            int[][] arr = new int[2*n-1][2*n-1];
+            int counterY = n-1;
+            int counterX;
+            for(int i=0; i< n ; i++) {
+                k = n;
+                counterX = n-1;
+                for(int j=0;j<n;j++) {
+                    
+                    arr[i][j] = k;
+                    if(k+i > n)
+                        k--;
+                }
+                for(int j=n-2; j>=0; j--) {
 
-    for(i=0;i<2*n-1;i++)
-        for(j=0;j<2*n-1;j++)
-            System.out.print(a[i][j]);
+                    //System.out.println(counterX);
+                    arr[i][++counterX] = arr[i][j];
+                }
+            }
+            for(int i=n-2; i>=0;i--) {
+                counterY++;
+                for(int j=0;j< 2*n-1; j++)
+                    arr[counterY][j] = arr[i][j];            
+            }
+
+            for(int i=0;i<2*n-1;i++) {
+                for(int j=0;j<2*n-1;j++)
+                    System.out.print(arr[i][j]);
+
+                System.out.println();
+            }
+        }
 }
